00707f2baee1e14547212e8d93c5c6f9fc005d21
gerritjvv <gerritjvv@gmail.com>
2013-12-24 11:57:24 +0100

    update client error handling on start

--- clj-tcp/project.clj
+++ clj-tcp/project.clj
@@ -1,4 +1,4 @@
-(defproject clj-tcp "0.1.1-SNAPSHOT"
+(defproject clj-tcp "0.1.2-SNAPSHOT"
   :description "Fast clojure tcp library based on netty"
   :url "https://github.com/gerritjvv/clj-tcp"
   :license {:name "Eclipse Public License"
