c1c9d02521146568e9527824d42611196697cbdf
gerritjvv <gerritjvv@gmail.com>
2013-12-28 10:33:27 +0100

    add external error-ch and remove reflection errors

---
+++ clj-tcp/java/clj_tcp/util/PipelineUtil.java
@@ -0,0 +1,20 @@
+package clj_tcp.util;
+
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelPipeline;
+import io.netty.util.concurrent.EventExecutorGroup;
+
+import java.util.Collection;
+
+/**
+ * 
+ * A util to avoid type casting
+ */
+public class PipelineUtil {
+
+	
+	public static final void addLast(ChannelPipeline pipeline, EventExecutorGroup group, Collection<ChannelHandler> handlers){
+		pipeline.addLast(group, handlers.toArray(new ChannelHandler[0]));
+	}
+	
+}
